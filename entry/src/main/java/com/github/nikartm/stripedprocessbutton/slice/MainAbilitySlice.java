b5b65ff126bd9aeac6a8e78474343e8ee01736b7
alpesh12345 <alpesh.kaushal@gmail.com>
2021-07-13 13:15:08 +0530

    final code

--- entry/src/main/java/com/github/nikartm/stripedprocessbutton/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/github/nikartm/stripedprocessbutton/slice/MainAbilitySlice.java
@@ -1,35 +1,48 @@
+/*
+ * Copyright (C) 2020-21 Application Library Engineering Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.nikartm.stripedprocessbutton.slice;
 
-import com.github.nikartm.stripedprocessbutton.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 import ohos.agp.components.Button;
+import com.github.nikartm.stripedprocessbutton.ResourceTable;
 import com.github.nikartm.support.StripedProcessButton;
 
+/**
+ * MainAbilitySlice.
+ */
 public class MainAbilitySlice extends AbilitySlice {
-    private StripedProcessButton stripedButton;
-    private Button btnStop;
-    private StripedProcessButton btn2;
-    private StripedProcessButton btn3;
-    private StripedProcessButton btn4;
-
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
-        stripedButton = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_start);
-        btnStop = (Button) findComponentById(ResourceTable.Id_btn_stop);
-        btn2 = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_2);
-        btn3 = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_3);
-        btn4 = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_4);
-        btn3.start();
-        btn4.start();
+        StripedProcessButton stripedButton = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_start);
+        StripedProcessButton button2 = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_2);
         stripedButton.setClickedListener(v -> stripedButton.start());
-        btn2.setClickedListener(v -> btn2.start());
+        button2.setClickedListener(v -> button2.start());
+        StripedProcessButton button3 = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_3);
+        StripedProcessButton button4 = (StripedProcessButton) findComponentById(ResourceTable.Id_btn_4);
+        Button btnStop = (Button) findComponentById(ResourceTable.Id_btn_stop);
         btnStop.setClickedListener(component -> {
             stripedButton.stop();
-            btn2.stop();
+            button2.stop();
         });
+        button3.start();
+        button4.start();
     }
 
     @Override
